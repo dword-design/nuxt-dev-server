4b791417388e42e5917463219ddab636e0b86dd0
Sebastian Landwehr <info@sebastianlandwehr.com>
2023-03-04 03:02:17 +0100

    fix: use nuxi (#2)

--- src/index.spec.js
+++ src/index.spec.js
@@ -10,6 +10,28 @@ import self from './index.js'
 
 export default tester(
   {
+    config: async () => {
+      await outputFiles({
+        'package.json': JSON.stringify({ type: 'module' }),
+        'pages-new/index.vue': endent`
+          <template>
+            <div class="foo" />
+          </template>
+        `,
+      })
+
+      const devServer = await self({
+        config: {
+          modules: [() => fs.outputFile('foo.txt', '')],
+          telemetry: false,
+        },
+      })
+      try {
+        expect(await fs.exists('foo.txt')).toEqual(true)
+      } finally {
+        await devServer.close()
+      }
+    },
     async works() {
       await outputFiles({
         'package.json': JSON.stringify({ type: 'module' }),
@@ -20,19 +42,22 @@ export default tester(
         `,
       })
 
-      const nuxt = await self()
-      await this.page.goto('http://localhost:3000')
-      await this.page.waitForSelector('.foo')
-      await fs.outputFile(
-        P.join('pages', 'index.vue'),
-        endent`
-      <template>
-        <div class="bar" />
-      </template>
-    `
-      )
-      await this.page.waitForSelector('.bar')
-      await nuxt.close()
+      const devServer = await self({ config: { telemetry: false } })
+      try {
+        await this.page.goto('http://localhost:3000')
+        await this.page.waitForSelector('.foo')
+        await fs.outputFile(
+          P.join('pages', 'index.vue'),
+          endent`
+        <template>
+          <div class="bar" />
+        </template>
+      `
+        )
+        await this.page.waitForSelector('.bar')
+      } finally {
+        await devServer.close()
+      }
     },
   },
   [testerPluginTmpDir(), testerPluginPuppeteer()]
